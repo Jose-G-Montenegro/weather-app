bae40420432ced81a7075503e461da80fb3dd447
Jocho94 <jocho1990@hotmail.com>
2021-11-23 21:06:53 -0300

    cambios para deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from "react-router-dom";
 import './App.css';
+import axios from 'axios';
+
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
 import About from '../components/About.jsx';
@@ -13,36 +15,34 @@ function App() {
   function onClose(id) {
     setCities(oldCities => oldCities.filter(c => c.id !== id));
   }
-  function onSearch(ciudad) {
+  async function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
-      .then(r => r.json())
-      .then((recurso) => {
-        if (recurso.main !== undefined) {
-          const ciudad = {
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: Math.round(recurso.main.temp),
-            feels_like: Math.round(recurso.main.feels_like),
-            hum: recurso.main.humidity,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
-          };
-          !!cities.find(e => e.name === ciudad.name) ?
-            alert("Ciudad ya agregada") :
-            setCities(oldCities => [...oldCities, ciudad]);
-            console.log(cities)
-        }
-        else {
-          alert("Ciudad no encontrada");
-        }
-      });
+    var response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`);
+    var recurso = response.data;
+    if (recurso.main !== undefined) {
+      const ciudad = {
+        min: Math.round(recurso.main.temp_min),
+        max: Math.round(recurso.main.temp_max),
+        img: recurso.weather[0].icon,
+        id: recurso.id,
+        wind: recurso.wind.speed,
+        temp: Math.round(recurso.main.temp),
+        feels_like: Math.round(recurso.main.feels_like),
+        hum: recurso.main.humidity,
+        name: recurso.name,
+        weather: recurso.weather[0].main,
+        clouds: recurso.clouds.all,
+        latitud: recurso.coord.lat,
+        longitud: recurso.coord.lon
+      };
+      !!cities.find(e => e.name === ciudad.name) ?
+        alert("Ciudad ya agregada") :
+        setCities(oldCities => [...oldCities, ciudad]);
+      console.log(cities)
+    }
+    else {
+      alert("Ciudad no encontrada");
+    }
   }
   function onFilter(ciudadId) {
     let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
